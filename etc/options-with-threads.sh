8e9d73b9e2e14a11dd23eaf59b746449edd18197
Mark Price <markprice@improbable.io>
2017-03-27 09:45:59 +0100

    add config files

---
+++ etc/options-with-threads.sh
@@ -0,0 +1,4 @@
+export PERF_COLLAPSE_OPTS="--kernel --tid"
+export FLAMEGRAPH_DIR=/home/mark/dev/FlameGraph/
+export PERF_MAP_OPTIONS="unfoldall"
+export PERF_JAVA_TMP=./tmp
---
+++ etc/options-without-threads.sh
@@ -0,0 +1,4 @@
+export PERF_COLLAPSE_OPTS="--kernel"
+export FLAMEGRAPH_DIR=/home/mark/dev/FlameGraph/
+export PERF_MAP_OPTIONS="unfoldall"
+
