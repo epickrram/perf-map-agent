3553212a3c74d718c65bc8d56f11c4c70daa0527
Mark Price <markprice@improbable.io>
2017-03-28 09:26:30 +0100

    remove local info

--- etc/options-with-threads.sh
+++ etc/options-with-threads.sh
@@ -1,4 +1,3 @@
 export PERF_COLLAPSE_OPTS="--kernel --tid"
-export FLAMEGRAPH_DIR=/home/mark/dev/FlameGraph/
 export PERF_MAP_OPTIONS="unfoldall"
 export PERF_JAVA_TMP=./tmp
--- etc/options-without-threads.sh
+++ etc/options-without-threads.sh
@@ -1,4 +1,3 @@
 export PERF_COLLAPSE_OPTS="--kernel"
-export FLAMEGRAPH_DIR=/home/mark/dev/FlameGraph/
 export PERF_MAP_OPTIONS="unfoldall"
 
